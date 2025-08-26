66fad9ccea8a29b55c36ed6b007b6e038559aa27
70001390 <ariesdwiprasetiyo4@gmail.com>
2025-08-26 12:43:49 +0700

    update

--- lib/widgets/template/tpe_login_page_tw.dart
+++ lib/widgets/template/tpe_login_page_tw.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:reactive_forms/reactive_forms.dart';
-import 'package:top_snackbar_flutter/top_snack_bar.dart';
-import 'package:tpe_component_sdk/utils/top_modal.dart';
 import 'package:tpe_login_sdk/tpe_login_sdk.dart'; // Assuming this contains showTPELoginBottomSheet
 
 class TpeLoginPageTW extends StatefulWidget {
@@ -20,27 +18,10 @@ class _LoginPageState extends State<TpeLoginPageTW> {
       loginType: 'tw',
       titleText: "Login TW",
       onSaveSuccess: (data) {
-<<<<<<< HEAD
-        if ("${data['username']}" != "Farischa" &&
-            "${data['password']}" != "Farischa123") {
-          showTopSnackBar(
-              Overlay.of(context),
-              TopModal.error(
-                  message:
-                      "Incorrect Password or Username. Please Try and Check again"));
-
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-                content:
-                    Text("username should : Farischa, password : Farischa123")),
-          );
-        }
-=======
         print("ID Card: ${data['idcard']}");
         print("Username: ${data['username']}");
         print("Password: ${data['password']}");
         print("Checkbox: ${data['checkbox']}");
->>>>>>> f619c88 (Update)
       },
     );
   }
